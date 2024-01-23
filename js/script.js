1e3b49e6b5de0e7ad70d1d033ae1db3750fabf6d
estebonbon <estesarmiento1@gmail.com>
2024-01-23 10:12:35 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+var button = document.querySelector(".got-this");
+
+button.addEventListener("click", function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this";
+});
+
+modalX.addEventListener("click", function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+});
